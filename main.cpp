5e4d2fc00389133c464ae78a38c5aee34946b0bb
Lelovias <46198166+Lelovias@users.noreply.github.com>
2019-03-13 21:30:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
+String rrz;
+
+
+char rz = a;
+char abc;
 int z1 = 0;
 int z2 = 0;
-
-
 int z1b = 0;
 int e = 0;
 
 
 
-char abc;
-
 void setup() {
 
   Serial.begin(9600);
   Serial.println("Type firts Number");
-  e = z1+z2;
 
 }
 
@@ -24,27 +24,37 @@ void loop() {
 
 
 
-    if (Serial.available() && z1 == 0 && z1b != 1) {
+    if (Serial.available() && z1 == 0) {
 
-      int z1 = Serial.parseInt();
+      z1 = Serial.parseInt();
       z1b = 1;
       Serial.println("Zahl 1: ");
       Serial.println(z1);
       }
 
 
-    if (Serial.available() && z1b == 1 && z2 == 0) {
 
+
+    if (Serial.available() && z1 != 0 && rz == "a") {
+
+        char rz = Serial.readStringUntil(abc);
+
+        Serial.println(rz);
+      }
+
+
+
+
+    if (Serial.available() &&  z1 != 0 && rz != "a" && z2 == 0) {
       int z2 = Serial.parseInt();
 
       Serial.println("Zahl 2: ");
       Serial.println(z2);
 
 
-
+      e = z1+z2;
       Serial.println("Ergebnis: ");
       Serial.println(e);
-      Serial.println(z1+z2);
 
 
 
