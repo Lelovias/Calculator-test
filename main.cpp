0970cf701c4612f3fce10ce8457246dea4976bfe
Lelovias <46198166+Lelovias@users.noreply.github.com>
2019-03-13 21:16:07 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+
+int z1 = 0;
+int z2 = 0;
+
+
+int z1b = 0;
+int e = 0;
+
+
+
+char abc;
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("Type firts Number");
+  e = z1+z2;
+
+}
+
+void loop() {
+
+
+
+
+    if (Serial.available() && z1 == 0 && z1b != 1) {
+
+      int z1 = Serial.parseInt();
+      z1b = 1;
+      Serial.println("Zahl 1: ");
+      Serial.println(z1);
+      }
+
+
+    if (Serial.available() && z1b == 1 && z2 == 0) {
+
+      int z2 = Serial.parseInt();
+
+      Serial.println("Zahl 2: ");
+      Serial.println(z2);
+
+
+
+      Serial.println("Ergebnis: ");
+      Serial.println(e);
+      Serial.println(z1+z2);
+
+
+
+      z1 = 0;
+      z2 = 0;
+      z1b = 0;
+    }
+
+
+
+}
